aeb7f4eccd9db9cb021b4d0a88d247662c30e7f5
Will Stanton <will@MacBook-Pro.local>
2016-10-18 15:18:40 -0400

    added tests and temporary view

--- src/restaurant/list/list.js
+++ src/restaurant/list/list.js
@@ -3,11 +3,54 @@ import Map from 'can/map/';
 import 'can/map/define/';
 import './list.less!';
 import template from './list.stache!';
+import Restaurant from 'place-my-order/models/restaurant';
+import State from 'place-my-order/models/state';
+import City from 'place-my-order/models/city';
 
 export const ViewModel = Map.extend({
   define: {
-    message: {
-      value: 'This is the pmo-restaurant-list component'
+    states: {
+    	get() {
+    		return State.getList({});
+    	}
+    },
+    state: {
+    	type: 'string',
+    	value: null,
+    	set() {
+    		//remove the city when the state changes
+    		this.attr('city', null);
+    	}
+    },
+    cities: {
+    	get() {
+    		let state = this.attr('state');
+
+    		if (!state) {
+    			return null;
+    		}
+
+    		return City.getList({ state });
+    	}
+    },
+    city: {
+    	type: 'string',
+    	value: null
+    },
+    restaurants: {
+    	get() {
+    		let state = this.attr('state');
+    		let city = this.attr('city');
+
+    		if (state && city) {
+    			return Restaurant.getList({
+    				'address.state': state,
+    				'address.city': city
+    			});
+    		}
+
+    		return null;
+    	}
     }
   }
 });
